5de4e6324736714bb594344fd9463e2a635f86b6
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-20 16:22:56 -0500

    ajuste nombre descargable

--- src/config/global.js
+++ src/config/global.js
@@ -122,7 +122,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/Anexos.zip',
+        download: 'downloads/623604_CF08_DU.zip',
       },
       {
         icono: 'fas fa-download',
