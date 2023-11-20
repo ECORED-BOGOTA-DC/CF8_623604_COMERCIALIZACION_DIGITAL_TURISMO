76669a983cf58486e2c105e5cfc80c470248d133
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-20 07:51:57 -0500

    Ajuste CFA e imagen

--- src/config/global.js
+++ src/config/global.js
@@ -124,11 +124,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/623604_CF08_DU.pdf',
       },
-      {
-        icono: 'fas fa-download',
-        titulo: 'Descargar material',
-        download: 'downloads/material.zip',
-      },
+      // {
+      //   icono: 'fas fa-download',
+      //   titulo: 'Descargar material',
+      //   download: 'downloads/material.zip',
+      // },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
